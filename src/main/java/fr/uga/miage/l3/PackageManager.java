a3951d7d3b047d14836761bd601b9891000a9b9a
nicomr70 <nicomr70@gmail.com>
2025-12-10 23:36:11 +0100

    feat(merge): apply little change 5

--- src/main/java/fr/uga/miage/l3/PackageManager.java
+++ src/main/java/fr/uga/miage/l3/PackageManager.java
@@ -1,17 +1,22 @@
 package fr.uga.miage.l3;
 
+import lombok.Getter;
+
 import java.util.Map;
 
+@Getter
 public class PackageManager {
-    private String name;
-    private String version;
-    private boolean isPrivate;
+    private String name = "demo-app";
+    private String version = "2.0.0";
+    private boolean isPrivate = true;
+
     private Map<String, String> dependencies = Map.of(
-            "a mettre les ", "dépendance"
+            "lodash", "^4.17.21",
+            "dayjs", "^1.11.10"
     );
+
     private Map<String, String> devDependencies = Map.of(
-            "a mettre les ", "dépendance"
+            "eslint", "^9.0.0",
+            "prettier", "^3.2.0"
     );
-
 }
-
