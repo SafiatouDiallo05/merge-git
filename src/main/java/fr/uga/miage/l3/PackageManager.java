aec06c0376922e2528a6035a00f582fcbd05858a
nicomr70 <nicomr70@gmail.com>
2025-12-10 23:51:36 +0100

    feat(merge): apply little rebase 1

--- src/main/java/fr/uga/miage/l3/PackageManager.java
+++ src/main/java/fr/uga/miage/l3/PackageManager.java
@@ -1,17 +1,26 @@
 package fr.uga.miage.l3;
 
+import lombok.Getter;
+
 import java.util.Map;
 
+@Getter
 public class PackageManager {
-    private String name;
-    private String version;
-    private boolean isPrivate;
+    private String name = "extended-app";
+    private String version = "3.5.1";
+    private boolean isPrivate = true;
+
     private Map<String, String> dependencies = Map.of(
-            "a mettre les ", "dépendance"
-    );
-    private Map<String, String> devDependencies = Map.of(
-            "a mettre les ", "dépendance"
+            "@angular/core", "^20.4.0",
+            "@angular/router", "^20.4.0",
+            "@angular/platform-browser", "^20.4.0",
+            "rxjs", "~7.9.0",
+            "tslib", "^2.4.0"
     );
 
+    private Map<String, String> devDependencies = Map.of(
+            "@angular/cli", "^20.4.0",
+            "@types/jasmine", "~5.2.0"
+    );
 }
 
